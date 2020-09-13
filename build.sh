92058aa651c70c3b9c3b8d5314d135c7a6b4d5d1
Callum Fox <cal-f@hotmail.com>
2020-09-13 12:14:50 +0100

    Add multi-arch build script.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -u
+
+export GOPATH=$(cd ../../ && pwd)
+TAG=${1:-v2.1.1}
+
+make build
+
+docker buildx build -t calfox/external-snapshotter:${TAG} --platform linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6 --push .
